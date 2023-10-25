bfd7644761f5218b302a0dd08537666d0f83aedf
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2023-10-25 23:18:03 +0200

    initial commit

---
+++ src/test/java/io/github/hrashk/students/cli/app/StudentsApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.hrashk.students.cli.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class StudentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
