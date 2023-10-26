7875f890a9d1d17c54f8fdd0787f94c071197e0d
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2023-10-26 15:45:09 +0200

    add a working integration test

--- src/main/java/io/github/hrashk/students/cli/app/StudentsApplication.java
+++ src/main/java/io/github/hrashk/students/cli/app/StudentsApplication.java
@@ -2,8 +2,10 @@ package io.github.hrashk.students.cli.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.CommandScan;
 
 @SpringBootApplication
+@CommandScan
 public class StudentsApplication {
 
     public static void main(String[] args) {
--- src/test/java/io/github/hrashk/students/cli/app/StudentsApplicationTests.java
+++ src/test/java/io/github/hrashk/students/cli/app/StudentsApplicationTests.java
@@ -1,12 +1,54 @@
 package io.github.hrashk.students.cli.app;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.shell.command.annotation.EnableCommand;
+import org.springframework.shell.test.ShellAssertions;
+import org.springframework.shell.test.ShellScreenAssert;
+import org.springframework.shell.test.ShellTestClient;
+import org.springframework.shell.test.autoconfigure.ShellTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 
+import java.time.Duration;
+import java.util.stream.Stream;
+
+import static org.awaitility.Awaitility.await;
+
+@ShellTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+// the next two annotations are an ugly hack to make the integration tests work...
+@ContextConfiguration(classes = {StudentsApplicationTests.TestConfig.class})
+@EnableCommand(StudentsCommands.class)
 class StudentsApplicationTests {
 
+    @Autowired
+    ShellTestClient client;
+
     @Test
-    void contextLoads() {
+    void help() {
+        ShellTestClient.InteractiveShellSession session = client
+                .interactive()
+                .run();
+
+        await().atMost(Duration.ofSeconds(2)).untilAsserted(() -> {
+            ShellAssertions.assertThat(session.screen())
+                    .containsText("shell");
+        });
+
+        session.write(session.writeSequence().text("help").carriageReturn().build());
+
+        await().atMost(Duration.ofSeconds(2)).untilAsserted(() -> {
+            ShellScreenAssert assertion = ShellAssertions.assertThat(session.screen());
+            Stream.of("Students App", "show:")
+                    .forEach(assertion::containsText);
+        });
     }
 
+    @Configuration
+    @ComponentScan
+    public static class TestConfig {
+    }
 }
