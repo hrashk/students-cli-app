15ec711dad2840dea2ce8a1427f3e9e4eda21e57
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2023-10-29 20:20:23 +0100

    replace student record with lombok value

--- src/main/java/io/github/hrashk/students/cli/app/Student.java
+++ src/main/java/io/github/hrashk/students/cli/app/Student.java
@@ -1,6 +1,15 @@
 package io.github.hrashk.students.cli.app;
 
-public record Student(int id, String firstName, String lastName, int age) {
+import lombok.experimental.Accessors;
+
+@lombok.Value
+@Accessors(fluent = true)
+public class Student {
+    int id;
+    String firstName;
+    String lastName;
+    int age;
+
     @Override
     public String toString() {
         return "%3d | %-10s | %-9s | %d".formatted(id, firstName, lastName, age);
