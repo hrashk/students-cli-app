767a260db5a5836cc95b2389b993e49443668514
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2023-10-31 11:01:43 +0100

    collapse students integration test

--- src/test/java/io/github/hrashk/students/cli/app/StudentsApplicationTests.java
+++ src/test/java/io/github/hrashk/students/cli/app/StudentsApplicationTests.java
@@ -1,12 +1,33 @@
 package io.github.hrashk.students.cli.app;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.shell.command.annotation.EnableCommand;
+import org.springframework.shell.test.ShellAssertions;
+import org.springframework.shell.test.ShellScreenAssert;
 import org.springframework.shell.test.ShellTestClient;
 import org.springframework.shell.test.autoconfigure.ShellTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.time.Duration;
+import java.util.stream.Stream;
+
+import static org.awaitility.Awaitility.await;
 
 @ShellTest
-class StudentsApplicationTests extends IntegrationScenario {
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@EnableCommand(StudentsCommands.class)
+class StudentsApplicationTests {
+    static final Duration TIMEOUT = Duration.ofSeconds(2);
+
+    /** {@code @ShellTest} does not scan for components, so we have to do it explicitly */
+    @TestConfiguration
+    @ComponentScan
+    static class TestConfig {
+    }
 
     @Autowired
     ShellTestClient client;
@@ -22,14 +43,31 @@ class StudentsApplicationTests extends IntegrationScenario {
         waitForStudentsAppCommands(session);
     }
 
-    @Test
-    void showPrintsNothing() {
-        var session = client.interactive().run();
+    static void waitForPrompt(ShellTestClient.InteractiveShellSession session) {
+        waitForText(session, "shell");
+    }
 
-        waitForPrompt(session);
+    static void waitForText(ShellTestClient.InteractiveShellSession session, String text) {
+        await().atMost(TIMEOUT).untilAsserted(() -> ShellAssertions.assertThat(session.screen())
+                .containsText(text));
+    }
 
-        enterCommand(session, "show");
+    static void checkTextDoesNotAppear(ShellTestClient.InteractiveShellSession session, String text) {
+        await().during(TIMEOUT)
+                .atMost(TIMEOUT.plus(Duration.ofSeconds(1)))
+                .untilAsserted(() -> Assertions.assertThat(session.screen().lines())
+                        .noneMatch(l -> l.contains(text)));
+    }
 
-        checkTextDoesNotAppear(session, "first name");
+    static void enterCommand(ShellTestClient.InteractiveShellSession session, String command) {
+        session.write(session.writeSequence().text(command).carriageReturn().build());
+    }
+
+    static void waitForStudentsAppCommands(ShellTestClient.InteractiveShellSession session) {
+        await().atMost(TIMEOUT).untilAsserted(() -> {
+            ShellScreenAssert assertion = ShellAssertions.assertThat(session.screen());
+            Stream.of("Students App", "show:", "add:", "remove:", "erase:")
+                    .forEach(assertion::containsText);
+        });
     }
 }
